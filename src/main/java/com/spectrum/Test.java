8e84b2a14b063a6f7d678952068819b6ec6b05c8
Meenzino <86910955+Minzino@users.noreply.github.com>
2023-02-22 22:43:05 +0900

    test: 빌드 테스트

--- src/main/java/com/spectrum/Test.java
+++ src/main/java/com/spectrum/Test.java
@@ -2,6 +2,6 @@ package com.spectrum;
 
 public class Test {
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        System.out.println("Hello World1");
     }
 }
