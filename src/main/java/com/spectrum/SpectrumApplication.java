023356398571f3cafded7de3a90a29c46eef653c
Meenzino <86910955+Minzino@users.noreply.github.com>
2023-02-22 20:16:14 +0900

    Init: 프로젝트 세팅

---
+++ src/main/java/com/spectrum/SpectrumApplication.java
@@ -0,0 +1,13 @@
+package com.spectrum;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpectrumApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpectrumApplication.class, args);
+	}
+
+}
