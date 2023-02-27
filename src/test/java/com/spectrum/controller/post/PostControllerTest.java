e91c045b694507272f1b06c4bbe56b80d5eec082
Meenzino <86910955+Minzino@users.noreply.github.com>
2023-02-27 15:07:41 +0900

    test: Post 생성 성공 테스트 구현 & RestDoc 적용

---
+++ src/test/java/com/spectrum/controller/post/PostControllerTest.java
@@ -0,0 +1,43 @@
+package com.spectrum.controller.post;
+
+import static io.restassured.RestAssured.given;
+import static org.springframework.restdocs.payload.PayloadDocumentation.fieldWithPath;
+import static org.springframework.restdocs.payload.PayloadDocumentation.requestFields;
+import static org.springframework.restdocs.restassured3.RestAssuredRestDocumentation.document;
+
+import com.spectrum.controller.post.dto.PostCreateRequest;
+import com.spectrum.documentationtest.InitIntegrationDocsTest;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.restdocs.payload.JsonFieldType;
+
+
+public class PostControllerTest extends InitIntegrationDocsTest {
+
+    @Test
+    @DisplayName("게시글 생성 요청이 정상적인 경우라면 게시글 생성 후 상태코드를 반환한다.")
+    void create_post_success() {
+        PostCreateRequest request = new PostCreateRequest("title", "content");
+
+        given(this.spec)
+            .filter(
+                document("post-create",
+                    requestFields(
+                        fieldWithPath("title").description("게시글 제목").type(JsonFieldType.STRING),
+                        fieldWithPath("content").description("게시글 내용").type(JsonFieldType.STRING)
+                    )
+                )
+            )
+            .accept(MediaType.APPLICATION_JSON_VALUE)
+            .header("Content-type", MediaType.APPLICATION_JSON_VALUE)
+            .body(request)
+
+        .when()
+            .post("/api/posts")
+
+        .then()
+            .statusCode(HttpStatus.CREATED.value());
+    }
+}
