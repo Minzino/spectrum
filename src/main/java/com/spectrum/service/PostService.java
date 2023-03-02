fa1dc0553754f75d1be100a8da968cbedb0f1cb3
Meenzino <86910955+Minzino@users.noreply.github.com>
2023-03-03 02:14:41 +0900

    refactor: post update 코드 수정 - save 메서드 제거

--- src/main/java/com/spectrum/service/PostService.java
+++ src/main/java/com/spectrum/service/PostService.java
@@ -3,10 +3,10 @@ package com.spectrum.service;
 import com.spectrum.controller.post.dto.PostCreateResponse;
 import com.spectrum.controller.post.dto.PostUpdateResponse;
 import com.spectrum.domain.post.Post;
+import com.spectrum.exception.post.PostNotFoundException;
 import com.spectrum.repository.PostRepository;
 import com.spectrum.service.dto.PostCreateDto;
 import com.spectrum.service.dto.PostUpdateDto;
-import java.util.NoSuchElementException;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -30,10 +30,10 @@ public class PostService {
     @Transactional
     public PostUpdateResponse update(Long memberId, Long postId, PostUpdateDto postUpdateDto) {
 
-        Post findPost = postRepository.findById(postId)
-            .orElseThrow(() -> new NoSuchElementException("존재하지 않는 게시글입니다."));
+        Post post = postRepository.findById(postId)
+            .orElseThrow(PostNotFoundException::new);
 
-        Post updatePost = postRepository.save(findPost.update(postUpdateDto, memberId));
-        return PostUpdateResponse.ofEntity(updatePost);
+        post.update(postUpdateDto, memberId);
+        return PostUpdateResponse.ofEntity(post);
     }
 }
