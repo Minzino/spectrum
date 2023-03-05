95ccfd9643959c4f6795bed0b66ee8a1de14dee2
Meenzino <86910955+Minzino@users.noreply.github.com>
2023-03-05 23:01:06 +0900

    refactor: @Slf4j로 변경

--- src/main/java/com/spectrum/common/exception/GlobalExceptionHandler.java
+++ src/main/java/com/spectrum/common/exception/GlobalExceptionHandler.java
@@ -3,8 +3,7 @@ package com.spectrum.common.exception;
 import com.spectrum.common.dto.ErrorResponse;
 import java.util.List;
 import java.util.stream.Collectors;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.BindingResult;
@@ -13,9 +12,8 @@ import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
 @RestControllerAdvice
+@Slf4j
 public class GlobalExceptionHandler {
-
-    private final Logger log = LoggerFactory.getLogger(this.getClass());
 
     private final ErrorMapping errorMapping;
 
