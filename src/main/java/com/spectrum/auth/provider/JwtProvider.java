a3cb94875dcdc28123f1e6f9056742e8d6ecd7ee
Meenzino <86910955+Minzino@users.noreply.github.com>
2023-03-18 03:58:43 +0900

    refactor: argument resolver에서 토큰 유효성 검사 - InvalidTokenException Exception 생성

--- src/main/java/com/spectrum/auth/provider/JwtProvider.java
+++ src/main/java/com/spectrum/auth/provider/JwtProvider.java
@@ -1,5 +1,7 @@
 package com.spectrum.auth.provider;
 
+import com.spectrum.exception.auth.InvalidTokenException;
+import com.spectrum.exception.auth.TokenExpiredException;
 import io.jsonwebtoken.Claims;
 import io.jsonwebtoken.ExpiredJwtException;
 import io.jsonwebtoken.Jws;
@@ -14,6 +16,7 @@ import org.springframework.stereotype.Component;
 
 @Component
 public class JwtProvider {
+
     @Value("${jwt.access-token.expire-length}")
     private long accessTokenValidityInMilliseconds;
     @Value("${jwt.refresh-token.expire-length}")
@@ -23,25 +26,18 @@ public class JwtProvider {
     public JwtProvider(@Value("${jwt.token.secret-key}") String secretKey) {
         this.secretKey = Keys.secretKeyFor(SignatureAlgorithm.HS256);
     }
+
     public String createAccessToken(String payload) {
-        return createToken(payload, accessTokenValidityInMilliseconds);
+        return createToken(String.valueOf(payload), accessTokenValidityInMilliseconds);
     }
 
     public String createRefreshToken(String payload) {
-        Claims claims = Jwts.claims().setSubject(payload);
-        Date now = new Date();
-        Date validity = new Date(now.getTime() + refreshTokenValidityInMilliseconds);
-
-        return Jwts.builder()
-            .setClaims(claims)
-            .setIssuedAt(now)
-            .setExpiration(validity)
-            .signWith(secretKey)
-            .compact();
+        return createToken(String.valueOf(payload), refreshTokenValidityInMilliseconds);
     }
 
     public String createToken(String payload, long expireLength) {
         Claims claims = Jwts.claims().setSubject(payload);
+        claims.put("id", payload);
         Date now = new Date();
         Date validity = new Date(now.getTime() + expireLength);
 
@@ -55,21 +51,31 @@ public class JwtProvider {
 
     public String getPayload(String token) {
         try {
-            return Jwts.parserBuilder().setSigningKey(secretKey).build().parseClaimsJws(token).getBody().getSubject();
+            return Jwts.parserBuilder()
+                .setSigningKey(secretKey)
+                .build()
+                .parseClaimsJws(token)
+                .getBody()
+                .getSubject();
         } catch (ExpiredJwtException e) {
-            return e.getClaims().getSubject();
+            throw new TokenExpiredException();
         } catch (JwtException e) {
-            throw new RuntimeException("유효하지 않은 토큰입니다.");
+            throw new InvalidTokenException();
         }
     }
 
     public boolean validateToken(String token) {
         try {
-            Jws<Claims> claims = Jwts.parserBuilder().setSigningKey(secretKey).build().parseClaimsJws(token);
-
+            Jws<Claims> claims = Jwts
+                .parserBuilder()
+                .setSigningKey(secretKey)
+                .build()
+                .parseClaimsJws(token);
             return !claims.getBody().getExpiration().before(new Date());
+        } catch (ExpiredJwtException e) {
+            throw new TokenExpiredException();
         } catch (JwtException | IllegalArgumentException e) {
-            return false;
+            throw new InvalidTokenException();
         }
     }
 }
--- src/main/java/com/spectrum/common/exception/ErrorCodeAndMessage.java
+++ src/main/java/com/spectrum/common/exception/ErrorCodeAndMessage.java
@@ -6,9 +6,15 @@ import org.springframework.http.HttpStatus;
 @Getter
 public enum ErrorCodeAndMessage {
 
-    SERVER_ERROR("S501 - Internal Server Error", "서버 에러입니다.", HttpStatus.INTERNAL_SERVER_ERROR),
-    INVALID_REQUEST("F402 - Invalid Request", "요청 정보를 확인해주세요.", HttpStatus.BAD_REQUEST),
-    POST_NOT_FOUND("P401 - Post Not Found", "게시글을 찾을 수 없습니다.", HttpStatus.NOT_FOUND);
+    SERVER_ERROR("S500 - Internal Server Error", "서버 에러입니다.", HttpStatus.INTERNAL_SERVER_ERROR),
+    INVALID_REQUEST("F400 - Invalid Request", "요청 정보를 확인해주세요.", HttpStatus.BAD_REQUEST),
+    POST_NOT_FOUND("P404 - Post Not Found", "게시글을 찾을 수 없습니다.", HttpStatus.NOT_FOUND),
+    INVALID_USER("U401 - Invalid User", "유효하지 않은 사용자입니다.", HttpStatus.UNAUTHORIZED),
+    USER_NOT_FOUND("U404 - User Not Found", "유저를 찾을 수 없습니다.", HttpStatus.NOT_FOUND),
+    NOT_AUTHOR("A403 - Not Author", "작성자가 아닙니다.", HttpStatus.FORBIDDEN),
+    TOKEN_EXPIRED("T401 - Token Expired", "토큰이 만료되었습니다.", HttpStatus.UNAUTHORIZED),
+    INVALID_TOKEN("T400 - Invalid Token", "유효하지 않은 토큰입니다.", HttpStatus.BAD_REQUEST),
+    HTTP_SERVLET_REQUEST_NULL("H400 - HttpServletRequest Null", "HttpServletRequest가 null입니다.", HttpStatus.BAD_REQUEST);
 
     private final String code;
     private final String message;
