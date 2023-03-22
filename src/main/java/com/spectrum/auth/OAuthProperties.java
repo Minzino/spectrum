5ef69b71487c7b58d53775f0e50b58eb49840ea2
Meenzino <86910955+Minzino@users.noreply.github.com>
2023-03-23 07:52:45 +0900

    refactor: OAuthProperties에 Validated 적용

--- src/main/java/com/spectrum/auth/OAuthProperties.java
+++ src/main/java/com/spectrum/auth/OAuthProperties.java
@@ -3,16 +3,22 @@ package com.spectrum.auth;
 import com.spectrum.auth.dto.ProviderPropertiesDto;
 import com.spectrum.auth.dto.UserPropertiesDto;
 import java.util.Map;
+import javax.validation.constraints.NotNull;
 import lombok.Getter;
 import lombok.RequiredArgsConstructor;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.boot.context.properties.ConstructorBinding;
+import org.springframework.validation.annotation.Validated;
 
 @Getter
 @ConstructorBinding
+@Validated
 @RequiredArgsConstructor
 @ConfigurationProperties(prefix = "oauth2")
 public class OAuthProperties {
+
+    @NotNull
     private final Map<String, UserPropertiesDto> user;
+    @NotNull
     private final Map<String, ProviderPropertiesDto> provider;
 }
