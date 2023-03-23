a0eb14f2c2d7ad52f0266d359cba195d63420da0
Meenzino <86910955+Minzino@users.noreply.github.com>
2023-03-24 01:02:12 +0900

    refactor: 테스트를 위해 생성자 생성

--- src/main/java/com/spectrum/controller/comment/dto/CommentCreateRequest.java
+++ src/main/java/com/spectrum/controller/comment/dto/CommentCreateRequest.java
@@ -15,6 +15,10 @@ public class CommentCreateRequest {
     @Size(max = 500)
     private String content;
 
+    public CommentCreateRequest(String content) {
+        this.content = content;
+    }
+
     public CommentCreateDto convertToCommentCreateDto() {
         return new CommentCreateDto(content);
     }
