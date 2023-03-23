922aba3b569ddda951c609aebbd31627d7cd9118
Meenzino <86910955+Minzino@users.noreply.github.com>
2023-03-23 21:24:54 +0900

    refactor: 응답 요청 필드 수정

--- src/main/java/com/spectrum/controller/comment/dto/RepliesCreateResponse.java
+++ src/main/java/com/spectrum/controller/comment/dto/RepliesCreateResponse.java
@@ -9,14 +9,25 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor(access = AccessLevel.PRIVATE)
 public class RepliesCreateResponse {
 
-    private Long commentId;
+    private Long parentId;
+    private Long userId;
+    private Long postId;
+    private String content;
 
-    private RepliesCreateResponse(Long commentId) {
-        this.commentId = commentId;
+    private RepliesCreateResponse(Long parentId, Long userId, Long postId,
+        String content) {
+        this.parentId = parentId;
+        this.userId = userId;
+        this.postId = postId;
+        this.content = content;
     }
 
     public static RepliesCreateResponse ofEntity(Comment replies) {
         return new RepliesCreateResponse(
-            replies.getId());
+            replies.getParentId()
+            , replies.getUserId()
+            , replies.getPostId()
+            , replies.getContent()
+        );
     }
 }
