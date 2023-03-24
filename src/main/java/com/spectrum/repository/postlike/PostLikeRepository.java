542a19bd9fdbd574c2b197a8ad4bd332934c18c9
Meenzino <86910955+Minzino@users.noreply.github.com>
2023-03-24 15:00:03 +0900

    feat: PostLikeRepository 구현

---
+++ src/main/java/com/spectrum/repository/postlike/PostLikeRepository.java
@@ -0,0 +1,8 @@
+package com.spectrum.repository.postlike;
+
+import com.spectrum.domain.postlike.PostLike;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface PostLikeRepository extends JpaRepository<PostLike,Long> {
+
+}
